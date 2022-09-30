629bcc417cd63a98da4171577e0def66f4e9e7c6
zhuzhenfeng.code <zhuzhenfeng.code@bytedance.com>
2022-09-30 23:25:58 +0800

    make op public
    Change-Id: Ifeff8a1fe578771d2569d11c8603c5fea82dc6e4

--- src/ops/file_op.rs
+++ src/ops/file_op.rs
@@ -1,19 +1,19 @@
 use deno_core::{anyhow::Result, error::AnyError, op};
 
 #[op]
-async fn op_read_file(path: String) -> Result<String, AnyError> {
+pub async fn op_read_file(path: String) -> Result<String, AnyError> {
     let contents = tokio::fs::read_to_string(path).await?;
     Ok(contents)
 }
 
 #[op]
-async fn op_write_file(path: String, contents: String) -> Result<(), AnyError> {
+pub async fn op_write_file(path: String, contents: String) -> Result<(), AnyError> {
     tokio::fs::write(path, contents).await?;
     Ok(())
 }
 
 #[op]
-fn op_remove_file(path: String) -> Result<(), AnyError> {
+pub fn op_remove_file(path: String) -> Result<(), AnyError> {
     std::fs::remove_file(path)?;
     Ok(())
 }
